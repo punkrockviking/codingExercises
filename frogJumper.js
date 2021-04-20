1716d8e0ab6816aac35e99e222e5933118d28e15
punkrockviking <danjeccles@gmail.com>
2021-04-19 22:58:03 -0600

    (feat) nearly solved, added jumpStones function, not working properly

--- frogJumper.js
+++ frogJumper.js
@@ -30,11 +30,11 @@ const bestStones = [0,1,3,5,8,12,17]
 // can the frog cross by jumping on every stone?
 // loop through each index
   // 0 index is starting point. .length -1 is ending point
-function canCrossUsingEveryStone(arr) {
-  for (let i = 1; i < arr.length - 1; i++) {
-    const prevJump = arr[i] - arr[i-1]
+function canCrossUsingEveryStone(stones) {
+  for (let i = 1; i < stones.length - 1; i++) {
+    const prevJump = stones[i] - stones[i-1]
     // check if next jump is -1, equal, or + 1
-    const nextJump = arr[i+1] - arr[i]
+    const nextJump = stones[i+1] - stones[i]
     if (!(nextJump === prevJump || nextJump === prevJump - 1 || nextJump === prevJump + 1)) {
       return false
     } 
@@ -42,25 +42,74 @@ function canCrossUsingEveryStone(arr) {
   return true
 }
 
-function canCrossBySkippingStones(arr) {
-    // 3 jump values
+function jumpStones(distance, stone, lastStone) {
+  const nextStone = distance + stone
+  if (nextStone === lastStone) {
+    return true
+  }
+  // return false if nextStone is not in the stones list
+  if (!stones.includes(nextStone)){
+    console.log(nextStone)
+    return false
+    // THIS IS WHERE IT FAILS, DOES NOT MOVE PAST HERE
+  }
+  // change the values of the current stone and jump distance and plug them in
 
+//   jumpDifferential = [-1, 0, 1]
+//   return jumpDifferential.some(relativeDistance => jumpStones(distance + relativeDistance, nextStone, lastStone))
+  
+//   return jumpStones(distance - 1, nextStone, lastStone) || jumpStones(distance, nextStone, lastStone) || jumpStones(distance + 1, nextStone, lastStone)
+
+  const sameJumpSucceeds = jumpStones(distance, nextStone, lastStone)
+  if (sameJumpSucceeds) {
+    return true
+  } 
+  const shortJumpSucceeds = jumpStones(distance - 1, nextStone, lastStone)
+  if (shortJumpSucceeds) {
+    return true
+  }
+  const longJumpSucceeds = jumpStones(distance + 1, nextStone, lastStone)
+  if (longJumpSucceeds) {
+    return true
+  }
+  return false
+}
+
+function canCrossBySkippingStones(stones) {
+    // 3 jump values
+  const lastStone = stones[stones.length-1]
+  const prevJump = 1
+  const currentStone = stones[1]
     // do any of the jumps lead to a stone that is forward in the river?
+  // does our jumpDistance +-1 lead to another stone?
+  // constraints: jump cant be 0, must lead to forward stone,
+  return jumpStones(prevJump, currentStone, lastStone)
+  
+    // if so, what do i do with that info?
     // jump as far as possible
     // helper function to make jumps
     // when a jump isnt possible, go back and follow another path
 }
 
-function canCross(arr) {
+function canCross(stones) {
   // if first jump is not 1, fail
-  if (arr[1] !== 1) {
+  if (stones[1] !== 1) {
     return false
   }
-  if (canCrossUsingEveryStone(arr)) return true
+  if (canCrossUsingEveryStone(stones)) return true
   // for each stone check the 3 jump values to see if they lead to another stone
-  if (canCrossBySkippingStones(arr)) return true
+  if (canCrossBySkippingStones(stones)) return true
 
   return false
 }
 
+
+// how to deal with early return on first jump
+// how to cache the stone we already visited
+
+
+
+console.log(canCross(stones))
+// console.log(canCross(bestStones))
+// console.log(canCross(failedStones))
 // canCrossUsingEveryStone(stones)
